41fe960d7b333bc7d4662383f9cb17a813ab36c9
rsaito83131 <rsaito831@gmail.com>
2018-09-25 20:32:24 +0900

    spruceのフロー追加

--- abw.tcl
+++ abw.tcl
@@ -12,6 +12,7 @@ proc finish {} {
     exit 0
 }
 
+# クロストラヒック(CBR)のパラメータ設定
 set bw 1000000000
 set size 1500
 #set interval 0.001
@@ -38,7 +39,9 @@ $ns duplex-link $n2 $n3 $bw 10ms DropTail
 $ns duplex-link $n4 $n3 3000Mb 10ms DropTail
 $ns duplex-link $n5 $n3 3000Mb 10ms DropTail
 
-
+##########################
+# クロストラヒックの設定 #
+# ########################
 # CBRのトラヒックジェネレータを利用した接続
 proc attach-cbr-traffic { node sink size rate } {
 	set ns [Simulator instance]
@@ -59,7 +62,6 @@ proc attach-cbr-traffic { node sink size rate } {
 	$ns connect $source $sink
 	return $traffic
 }
-
 
 # 可用帯域計算及び記録
 proc record {} {
@@ -85,19 +87,40 @@ proc record {} {
     $ns at [expr $now+$time] "record"
 }
 
-
 # sinkの定義
 set sink0 [new Agent/LossMonitor]
 $ns attach-agent $n3 $sink0
 
-
 # クロストラヒックフローを定義
 set source0 [attach-cbr-traffic $n0 $sink0 $size $rate]
+
+################
+# Spruceの設定 #
+################
+# Seder side 
+set sender [new Agent/spruce_sndr]
+$ns attach-agent $n1 $sender
+$sender set WaitInterval_ .001
+$sender set packetSize_ 1500
+$sender set NumPacks_ 2
+$sender set Max_rate_ 1000000000
+
+# Receiver side
+set recv [new Agent/spruce_rcvr]
+$ns attach-agent $n5 $recv
+$recv set packetSize_ 1500
+$recv set NumPacks_ 2
+$recv set samples_for_ab_ 100
+
+#senderとrecvのフローを定義
+$ns connect $sender $recv
 
 
 # シミューレーション設定
 $ns at 0.0 "record"
 $ns at 0.5 "$source0 start"
+$ns at 0.5 "$sender start"
+$ns at 0.5 "$recv start"
 $ns at 400.0 "$source0 stop"
 $ns at 400.0 "finish"
 $ns run
